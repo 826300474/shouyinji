427536c006c6861edbbad979970321146cb9b31c
chai <826300474@qq.com>
2018-11-29 09:50:26 +0800

    6516516

--- src/renderer/api/index.js
+++ src/renderer/api/index.js
@@ -30,28 +30,10 @@ service.interceptors.request.use(config => {
 service.interceptors.response.use(
     response => {
         return response.data;
-        // if (response.data.code !== 200) {
-        //     switch (response.data.code) {
-        //         case 401: // 用户未登录
-        //             break;
-        //         default:
-        //             console.log(response.data.msg);
-        //             Message({
-        //                 message: response.data.msg || '未知错误',
-        //                 type: 'error',
-        //                 center: true
-        //             })
-        //             break;
-        //     }
-        //     return Promise.reject(response)
-        // } else {
-        //     return response.data
-        // }
     },
     error => {
         if (error.response) {
-            const res = error.response.data
-
+            const res = error.response.data;
             return Promise.reject(res)
         } else {
             return Promise.reject(error)
